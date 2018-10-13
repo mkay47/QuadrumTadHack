29ba43b14bf6b1a115ee4ca80508d3426136ea07
Mkhululi Mtshemla <Mkhululi.Mtshemla@psg.co.za>
2018-10-13 21:53:37 +0200

    sms model

--- API/DocketSystemAPI/DocketSystemAPI/Models/SMS.cs
+++ API/DocketSystemAPI/DocketSystemAPI/Models/SMS.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,13 @@ namespace DocketSystemAPI.Models
 {
     public class SMS
     {
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty("number")]
+        public string Number { get; set; }
     }
 }
