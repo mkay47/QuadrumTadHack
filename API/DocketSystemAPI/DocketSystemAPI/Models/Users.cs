b09fc7304ce0c7b077fe5e2ad9bd0d0792ae9001
tlekwadi <tslekwadi@gmail.com>
2018-10-13 12:50:55 +0200

    Basic models for the system

---
+++ API/DocketSystemAPI/DocketSystemAPI/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocketSystemAPI.Models
+{
+    
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string IDNumber { get; set; }
+        public int Salary { get; set; }
+    }
+}
